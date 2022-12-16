fe199b7597dc4f453df9fb03bcb2ea4a531ba26e
Abhinav Jangda <abhijangda@gmail.com>
2022-12-15 22:29:44 -0500

    HeapEvent has method and java class

--- points-to-analysis/pointsto/src/main/java/HeapEvent.java
+++ points-to-analysis/pointsto/src/main/java/HeapEvent.java
@@ -7,55 +7,73 @@ import java.util.*;
 import java.io.*;
 import java.util.zip.ZipInputStream;
 
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.*;
+import org.apache.bcel.generic.Type;
+import org.apache.bcel.util.*;
+
 public class HeapEvent {
   //TODO: Use constant table indices to represent class and method?
-  String method_;
+  Method method_;
   int bci_;
   long srcPtr_;
-  String srcClass_;
+  JavaClass srcClass_;
   long dstPtr_;
-  String dstClass_;
+  JavaClass dstClass_;
 
-  public HeapEvent(String method, int bci, long src, String srcClass, long dst, String dstClass) {
-      this.method_ = method;
-      this.bci_ = bci;
-      this.srcPtr_ = src;
-      this.srcClass_ = srcClass;
-      this.dstPtr_ = dst;
-      this.dstClass_ = dstClass;
+  public HeapEvent(Method method, int bci, long src, JavaClass srcClass, long dst, JavaClass dstClass) {
+    this.method_ = method;
+    this.bci_ = bci;
+    this.srcPtr_ = src;
+    this.srcClass_ = srcClass;
+    this.dstPtr_ = dst;
+    this.dstClass_ = dstClass;
   }
 
-  public static HeapEvent fromString(String repr) {
-      assert(repr.charAt(0) == '[' && repr.charAt(-1) == ']'); 
-      // System.out.println(": " + repr); 
-      String[] split = repr.split(",");
-      String method = split[0].substring(1).strip();
-      int bci = Integer.parseInt(split[1].strip());
-      String[] src = split[2].split(":");
-      String[] dst = split[3].substring(0, split[3].length() - 1).split(":");
-      return new HeapEvent(method, bci,
-                      Long.parseLong(src[0].strip()),
-                      src[1].strip(),
-                      Long.parseLong(dst[0].strip()),
-                      dst[1].strip());
+  public static HeapEvent fromString(String repr, JavaClassCollection classes) {
+    assert(repr.charAt(0) == '[' && repr.charAt(-1) == ']'); 
+    // System.out.println(": " + repr);
+    String[] split = repr.split(",");
+    String method = split[0].substring(1).strip();
+    Method m = classes.getMethod(method);
+    if (JavaClassCollection.methodToCare(method))
+      assert (m != null);
+    int bci = Integer.parseInt(split[1].strip());
+    String[] src = split[2].split(":");
+    JavaClass srcClass = classes.getClassForString(src[1].strip());
+    if (JavaClassCollection.classToCare(src[1].strip()))
+      assert(srcClass != null);
+
+    String[] dst = split[3].substring(0, split[3].length() - 1).split(":");
+
+    JavaClass dstClass = classes.getClassForString(dst[1].strip());
+    if (JavaClassCollection.classToCare(dst[1].strip()))
+      assert(dstClass != null);
+
+    return new HeapEvent(m, bci,
+                         Long.parseLong(src[0].strip()),
+                         srcClass,
+                         Long.parseLong(dst[0].strip()),
+                         dstClass);
   }
 
   //Parse heapevents file to create a map of each thread to a list of heap events
-  public static HashMap<String, ArrayList<HeapEvent>> processHeapEventsFile(String fileName) {
-      BufferedReader reader;
-      HashMap<String, ArrayList<HeapEvent>> heapEvents = new HashMap<String, ArrayList<HeapEvent>>();
-          try {
-              reader = new BufferedReader(new FileReader(fileName));
-              String line = reader.readLine();
+  public static HashMap<String, ArrayList<HeapEvent>> processHeapEventsFile(String fileName, JavaClassCollection classes) {
+    BufferedReader reader;
+    HashMap<String, ArrayList<HeapEvent>> heapEvents = new HashMap<String, ArrayList<HeapEvent>>();
+
+    try {
+      reader = new BufferedReader(new FileReader(fileName));
+      String line = reader.readLine();
       String currThread = "";
       ArrayList<HeapEvent> currEvents = null;
 
-              while (line != null) {
-                  if (line.charAt(0) == '[' && line.charAt(line.length() - 1) == ']') {
+      while (line != null) {
+        if (line.charAt(0) == '[' && line.charAt(line.length() - 1) == ']') {
           assert(currEvents != null);
-          HeapEvent he = HeapEvent.fromString(line);
+          HeapEvent he = HeapEvent.fromString(line, classes);
           currEvents.add(he);
-          } else if (line.contains(":")) {
+        } else if (line.contains(":")) {
           currThread = line.substring(0, line.indexOf(":"));
           if (heapEvents.containsKey(currThread) == false) {
               currEvents = new ArrayList<>();
@@ -63,21 +81,21 @@ public class HeapEvent {
           }
 
           currEvents = heapEvents.get(currThread);
-          }
+        }
 
-                  // read next line
-                  line = reader.readLine();
-              }
+        // read next line
+        line = reader.readLine();
+      }
 
-              reader.close();
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
+      reader.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
 
-      return heapEvents;
+    return heapEvents;
   }
 
   public String toString() {
-      return "[" + method_ + "," + Integer.toString(bci_) + "," + Long.toString(srcPtr_) + ":" + srcClass_ + "," + Long.toString(dstPtr_) + ":" + dstClass_ + "]";
+    return "[" + method_ + "," + Integer.toString(bci_) + "," + Long.toString(srcPtr_) + ":" + srcClass_ + "," + Long.toString(dstPtr_) + ":" + dstClass_ + "]";
   }
 }
--- points-to-analysis/pointsto/src/main/java/Main.java
+++ points-to-analysis/pointsto/src/main/java/Main.java
@@ -52,54 +52,6 @@ public class Main {
     return mainMethods;
   }
 
-      /**
-     * Extracts a zip entry (file entry)
-     * @param zipIn
-     * @param filePath
-     * @throws IOException
-     */
-    public static void extractFile(JarInputStream zipIn, String filePath) throws IOException {
-          BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
-          byte[] bytesIn = new byte[1024*1024];
-          int read = 0;
-          while ((read = zipIn.read(bytesIn)) != -1) {
-              bos.write(bytesIn, 0, read);
-          }
-          bos.close();
-      }
-  
-  public static void createMethodNameMap(String jarFile, HashMap<String, Method> methodNameMap) {
-    try {
-      JarFile jar = new JarFile(jarFile);
-      JarInputStream jarIn = new JarInputStream(new FileInputStream (jarFile));
-      JarEntry entry = jarIn.getNextJarEntry();
-      while (entry != null) {
-          if (entry.getName().endsWith(".class")) {
-            ClassParser parser = new ClassParser(jarIn, entry.getName());
-            JavaClass javaClass = parser.parse();
-            // System.out.println(javaClass.getClassName());
-            for (Method m : javaClass.getMethods()) {
-              String methodName = javaClass.getClassName() +
-                                  "." + m.getName() + m.getSignature();
-              // if (methodName.contains("org.apache.lucene.store.FSDirectory."))
-              //   System.out.println(methodName);
-              methodNameMap.put(methodName, m);
-            }
-          } else if (entry.getName().endsWith(".jar")) {
-            Path entryPath = Paths.get(entry.getName());
-            String extractedJarFile = "/tmp/"+entryPath.getFileName();
-            extractFile(jarIn, extractedJarFile);
-            createMethodNameMap(extractedJarFile, methodNameMap);
-          }
-          jarIn.closeEntry();
-          entry = jarIn.getNextJarEntry();
-      }
-      jarIn.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
   public static HashMap<Method, HashMap<Integer, String>> invokeBCInMethod = new HashMap<>();
   public static HashMap<Integer, String> findInvokeBytecode(Method method) {
     if (invokeBCInMethod.containsKey(method))
@@ -148,136 +100,137 @@ public class Main {
                                HashMap<String, ArrayList<HeapEvent>> heapEvents, 
                                String mainThread, int heapEventIdx) {
     //Check all methods of event in the main thread are in methodNameMap
-    ArrayList<HeapEvent> mainThreadEvents = heapEvents.get(mainThread);
+    // ArrayList<HeapEvent> mainThreadEvents = heapEvents.get(mainThread);
 
-    for (int i = heapEventIdx; i < mainThreadEvents.size(); i++) {
-      HeapEvent he = mainThreadEvents.get(i);
-      if (methodToCare(he.method_) && !methodNameMap.containsKey(he.method_)) {
-        System.out.println("not found: " + he.method_);
-      // } else if (methodNameMap.containsKey(he.method_)) {
-        // HashMap<Integer, String> invokeMethods = findInvokeBytecode(methodNameMap.get(he.method_));
-        // System.out.println(methodNameMap.get(he.method_).getCode().toString(true));
-        // for (Map.Entry<Integer, String> e : invokeMethods.entrySet())
-        //   System.out.println(e.getKey() + " " + e.getValue());
-      // }
-      }
-    }
+    // for (int i = heapEventIdx; i < mainThreadEvents.size(); i++) {
+    //   HeapEvent he = mainThreadEvents.get(i);
+    //   if (methodToCare(he.method_) && !methodNameMap.containsKey(he.method_)) {
+    //     System.out.println("not found: " + he.method_);
+    //   // } else if (methodNameMap.containsKey(he.method_)) {
+    //     // HashMap<Integer, String> invokeMethods = findInvokeBytecode(methodNameMap.get(he.method_));
+    //     // System.out.println(methodNameMap.get(he.method_).getCode().toString(true));
+    //     // for (Map.Entry<Integer, String> e : invokeMethods.entrySet())
+    //     //   System.out.println(e.getKey() + " " + e.getValue());
+    //   // }
+    //   }
+    // }
 
-    Stack<JavaStackElement> callStack;
-    HeapEvent prevHe = mainThreadEvents.get(heapEventIdx);
-    Method prevMeth = methodNameMap.get(prevHe.method_);
+    // Stack<JavaStackElement> callStack;
+    // HeapEvent prevHe = mainThreadEvents.get(heapEventIdx);
+    // Method prevMeth = methodNameMap.get(prevHe.method_);
     
-    System.out.println("starting from " + prevHe.method_);
-    for (int idx = heapEventIdx + 1; idx < mainThreadEvents.size(); idx++) {
-      for (int idx2 = idx; idx2 < mainThreadEvents.size(); idx2++) {
-        String nextMethod = mainThreadEvents.get(idx2).method_;
-        if (!nextMethod.equals(prevHe.method_) && methodToCare(nextMethod)) {
-          idx = idx2;
-          break;
-        }
-      }
+    // System.out.println("starting from " + prevHe.method_);
+    // for (int idx = heapEventIdx + 1; idx < mainThreadEvents.size(); idx++) {
+    //   for (int idx2 = idx; idx2 < mainThreadEvents.size(); idx2++) {
+    //     String nextMethod = mainThreadEvents.get(idx2).method_;
+    //     if (!nextMethod.equals(prevHe.method_) && methodToCare(nextMethod)) {
+    //       idx = idx2;
+    //       break;
+    //     }
+    //   }
       
-      HeapEvent he = mainThreadEvents.get(idx);
-      HashMap<Integer, String> invokeBC = findInvokeBytecode(prevMeth);
-      boolean found = false;
-      Stack<JavaStackElement> newCallStack = new Stack<>();
-      found = isMethodReachable(methodNameMap, newCallStack, prevHe.method_, 0, he.method_);
+    //   HeapEvent he = mainThreadEvents.get(idx);
+    //   HashMap<Integer, String> invokeBC = findInvokeBytecode(prevMeth);
+    //   boolean found = false;
+    //   Stack<JavaStackElement> newCallStack = new Stack<>();
+    //   found = isMethodReachable(methodNameMap, newCallStack, prevHe.method_, 0, he.method_);
 
-      if (!found) {
-        System.out.println("Didn't find " + he.method_);
-        System.out.println(prevMeth.getCode().toString(true));
-        break;
-      }
+    //   if (!found) {
+    //     System.out.println("Didn't find " + he.method_);
+    //     System.out.println(prevMeth.getCode().toString(true));
+    //     break;
+    //   }
 
-      prevHe = he;
-      prevMeth = methodNameMap.get(he.method_);
-      System.out.println("found: " + he.method_ + " " + newCallStack.size());
-    }
+    //   prevHe = he;
+    //   prevMeth = methodNameMap.get(he.method_);
+    //   System.out.println("found: " + he.method_ + " " + newCallStack.size());
+    // }
   }
 
   public static void main(String[] args) throws ClassFormatException, IOException {
-    //Read and process heap events
-    String heapEventsFile = "/mnt/homes/aabhinav/jdk/heap-events";
-    HashMap<String, ArrayList<HeapEvent>> heapEvents = HeapEvent.processHeapEventsFile(heapEventsFile);
-    System.out.println("HeapEvents loaded");
     //Read the jarfile
     String jarFile = "/mnt/homes/aabhinav/jdk/dacapo-9.12-MR1-bach.jar";
+    JavaClassCollection javaClasses = JavaClassCollection.createFromJar(jarFile);
     
+    //Read and process heap events
+    String heapEventsFile = "/mnt/homes/aabhinav/jdk/heap-events";
+    HashMap<String, ArrayList<HeapEvent>> heapEvents = HeapEvent.processHeapEventsFile(heapEventsFile, javaClasses);
+    System.out.println("HeapEvents loaded");
     
-    //Find all main methods in the jar and find that method in the heap events
-    // ArrayList<Method> mainMethods = findMainMethods(jarFile, jar);
-    HashMap<String, Method> methodNameMap = new HashMap<>();
-    createMethodNameMap(jarFile, methodNameMap);
-    String mainThread = "";
-    int heapEventIdx = -1;
-    String threadWithMaxEvents = "";
+    // //Find all main methods in the jar and find that method in the heap events
+    // // ArrayList<Method> mainMethods = findMainMethods(jarFile, jar);
+    // HashMap<String, Method> methodNameMap = new HashMap<>();
+    // createMethodNameMap(jarFile, methodNameMap);
+    // String mainThread = "";
+    // int heapEventIdx = -1;
+    // String threadWithMaxEvents = "";
 
-    for (String thread : heapEvents.keySet()) {
-      if (threadWithMaxEvents == "") {
-        threadWithMaxEvents = thread;
-      }
-      if (heapEvents.get(threadWithMaxEvents).size() < heapEvents.get(thread).size())
-        threadWithMaxEvents = thread;
+    // for (String thread : heapEvents.keySet()) {
+    //   if (threadWithMaxEvents == "") {
+    //     threadWithMaxEvents = thread;
+    //   }
+    //   if (heapEvents.get(threadWithMaxEvents).size() < heapEvents.get(thread).size())
+    //     threadWithMaxEvents = thread;
 
-      System.out.println(thread + ": " + heapEvents.get(thread).size() + " events");
-    }
+    //   System.out.println(thread + ": " + heapEvents.get(thread).size() + " events");
+    // }
 
-    System.out.println("threadWithMaxEvents " + threadWithMaxEvents);
+    // System.out.println("threadWithMaxEvents " + threadWithMaxEvents);
 
-    int ii = 0;
-    for (HeapEvent he : heapEvents.get(threadWithMaxEvents)) {
-      if (methodToCare(he.method_)) {
-        System.out.println(he.toString());
+    // int ii = 0;
+    // for (HeapEvent he : heapEvents.get(threadWithMaxEvents)) {
+    //   if (methodToCare(he.method_)) {
+    //     System.out.println(he.toString());
         
-        mainThread = threadWithMaxEvents;
-        heapEventIdx = ii;
-        break;
-      }
-      ii++;
-    }
-    // for (Method mainMethod : mainMethods) {
-    //   String mainClassName = mainMethod.getClass().getName();
-      // System.out.println(mainClassName);
-      // for (String thread : heapEvents.keySet()) {
-      //   int i = 0;
-      //   for (HeapEvent he : heapEvents.get(thread)) {
-      //     //TODO: also check for the class obtained from above
-      //     if (he.method_.contains(".main") && he.method_.contains("org.dacapo.harness.TestHarness.main")) { //&& he.method_.contains(mainClassName)) {
-      //       mainThread = thread;
-      //       heapEventIdx = i;
-      //       System.out.println(mainThread + " " + heapEventIdx + " " + he.toString());
-      //     }
-      //     i++;
-      //   }
-
-      //   // if (heapEventIdx != -1) break;
-      // }
+    //     mainThread = threadWithMaxEvents;
+    //     heapEventIdx = ii;
+    //     break;
+    //   }
+    //   ii++;
     // }
+    // // for (Method mainMethod : mainMethods) {
+    // //   String mainClassName = mainMethod.getClass().getName();
+    //   // System.out.println(mainClassName);
+    //   // for (String thread : heapEvents.keySet()) {
+    //   //   int i = 0;
+    //   //   for (HeapEvent he : heapEvents.get(thread)) {
+    //   //     //TODO: also check for the class obtained from above
+    //   //     if (he.method_.contains(".main") && he.method_.contains("org.dacapo.harness.TestHarness.main")) { //&& he.method_.contains(mainClassName)) {
+    //   //       mainThread = thread;
+    //   //       heapEventIdx = i;
+    //   //       System.out.println(mainThread + " " + heapEventIdx + " " + he.toString());
+    //   //     }
+    //   //     i++;
+    //   //   }
 
-    assert(mainThread != "");
-    assert(heapEventIdx != -1);
+    //   //   // if (heapEventIdx != -1) break;
+    //   // }
+    // // }
 
-    System.out.println(mainThread + " " + heapEvents.get(mainThread).get(heapEventIdx).toString());
+    // assert(mainThread != "");
+    // assert(heapEventIdx != -1);
 
-    callGraph(methodNameMap, heapEvents, mainThread, heapEventIdx);
-    // System.out.println("17\n");
-    // /*An existing class can be parsed with ClassParser */
-    // ClassParser parser=new ClassParser(App.class.getResourceAsStream("App.class"), "App.class");
-    // JavaClass javaClass=parser.parse();
+    // System.out.println(mainThread + " " + heapEvents.get(mainThread).get(heapEventIdx).toString());
+
+    // callGraph(methodNameMap, heapEvents, mainThread, heapEventIdx);
+    // // System.out.println("17\n");
+    // // /*An existing class can be parsed with ClassParser */
+    // // ClassParser parser=new ClassParser(App.class.getResourceAsStream("App.class"), "App.class");
+    // // JavaClass javaClass=parser.parse();
     
-    // System.out.println("*******Constant Pool*********");
-    // System.out.println(javaClass.getConstantPool());
+    // // System.out.println("*******Constant Pool*********");
+    // // System.out.println(javaClass.getConstantPool());
     
-    // System.out.println("*******Fields*********");
-    // System.out.println(Arrays.toString(javaClass.getFields()));
-    // System.out.println();
+    // // System.out.println("*******Fields*********");
+    // // System.out.println(Arrays.toString(javaClass.getFields()));
+    // // System.out.println();
     
-    // System.out.println("*******Methods*********");
-    // System.out.println(Arrays.toString(javaClass.getMethods()));
+    // // System.out.println("*******Methods*********");
+    // // System.out.println(Arrays.toString(javaClass.getMethods()));
     
-    // for(Method method:javaClass.getMethods()){
-    //     System.out.println(method);
-    //     System.out.println(method.getCode());
-    // }
+    // // for(Method method:javaClass.getMethods()){
+    // //     System.out.println(method);
+    // //     System.out.println(method.getCode());
+    // // }
   }
 }

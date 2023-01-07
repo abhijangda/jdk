b5909482d281c08b85bb13f6d87e3e839d9f7a98
Abhinav Jangda <abhijangda@gmail.com>
2023-01-07 16:31:47 -0500

    script to clear methods from events

---
+++ points-to-analysis/pointsto/src/scripts/clear-methods.py
@@ -0,0 +1,27 @@
+import sys
+
+def methodToCare(method):
+  return method != "NULL" and method.find("java.") != 0 and method.find("jdk.") != 0 and method.find("sun.") != 0 and "<clinit>" not in method
+
+f = open(sys.argv[1])
+output = ""
+
+for line in f.readlines():
+  if "{" in line:
+    output += line
+  elif line[0] == "[":
+    # print(line, line.split())
+    method = line.split(',')[1].strip()
+    if methodToCare(method):
+      # print(method)
+      # print(method == "NULL" and method.find("java.") == 0 and method.find("jdk.") == 0 and method.find("sun.") == 0 and "<clinit>" in method)
+      # sys.exit(0)
+      output += line
+
+f.close()
+
+# print(output)
+
+f = open(sys.argv[2], "w")
+f.write(output)
+f.close()
